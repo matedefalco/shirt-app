82f7118224ec9eaaf614e804ab092e5de91b7f4b
Suka <mateodefalco1@gmail.com>
2023-08-23 12:03:10 -0300

    State creation and Home Imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import {motion, AnimatePresence} from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import {headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation} from '../config/motion'
+import state from '../store'
+
 const Home = () => {
+    const snap = useSnapshot(state)
+
     return (
     <div>Home</div>
     )
