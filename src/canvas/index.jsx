b63db42c1bdba4e1ec15de4262b37df20880e0b5
Suka <mateodefalco1@gmail.com>
2023-08-23 15:56:26 -0300

    Uncomment components

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,20 +1,20 @@
 import { Canvas } from "@react-three/fiber"
 import { Environment, Center } from "@react-three/drei"
 import Shirt from "./Shirt"
-// import Backdrop from "./Backdrop"
-// import CameraRig from "./CameraRig"
+import Backdrop from "./Backdrop"
+import CameraRig from "./CameraRig"
 
 const CanvasModel = () => {
 	return (
 		<Canvas>
 			<ambientLight intensity={0.5} />
 			<Environment preset="city" />
-			{/* <CameraRig> */}
-			{/* <Backdrop /> */}
-			<Center>
-				<Shirt />
-			</Center>
-			{/* </CameraRig> */}
+			<CameraRig>
+				<Backdrop />
+				<Center>
+					<Shirt />
+				</Center>
+			</CameraRig>
 		</Canvas>
 	)
 }
