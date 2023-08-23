3d5c70a9a53efa23ec6062db1bab1edb286d3faa
Suka <mateodefalco1@gmail.com>
2023-08-23 16:28:06 -0300

    ColorPicker Fix

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,40 +1,21 @@
-import CustomButton from "./CustomButton"
+import React from "react"
+import { SketchPicker } from "react-color"
+import { useSnapshot } from "valtio"
 
-const FilePicker = ({ file, setFile, readFile }) => {
+import state from "../store"
+
+const ColorPicker = () => {
+	const snap = useSnapshot(state)
+
 	return (
-		<div className="filepicker-container">
-			<div className="flex-1 flex flex-col">
-				<input
-					id="file-upload"
-					type="file"
-					accept="image/*"
-					onChange={(e) => setFile(e.target.files[0])}
-				/>
-				<label htmlFor="file-upload" className="filepicker-label">
-					Upload File
-				</label>
-
-				<p className="mt-2 text-gray-500 text-xs truncate">
-					{file === "" ? "No file selected" : file.name}
-				</p>
-			</div>
-
-			<div className="mt-4 flex flex-wrap gap-3">
-				<CustomButton
-					type="outline"
-					title="Logo"
-					handleClick={() => readFile("logo")}
-					customStyles="text-xs"
-				/>
-				<CustomButton
-					type="filled"
-					title="Full"
-					handleClick={() => readFile("full")}
-					customStyles="text-xs"
-				/>
-			</div>
+		<div className="absolute left-full ml-3">
+			<SketchPicker
+				color={snap.color}
+				disableAlpha
+				onChange={(color) => (state.color = color.hex)}
+			/>
 		</div>
 	)
 }
 
-export default FilePicker
+export default ColorPicker
